2d292dffd2dd19cb52f0d7a8fb2c29f26939eb9b
sumukhah <sumukha214@gmail.com>
2020-04-13 01:37:55 +0530

    Wall Screen: Add Button to set default or custom background

---
+++ wall.js
@@ -0,0 +1,14 @@
+// const colors = [
+//   "Black",
+//   "Blue",
+//   "Brown",
+//   "Gray",
+//   "Green",
+//   "Pink",
+//   "Purple",
+//   "Orange",
+//   "Yellow",
+// ];
+
+// document.querySelector(".background-color").innerHTML =
+//   "<table> <tr>" + colors.map((color) => `<td>${color}</td>`) + "</tr></table>";
